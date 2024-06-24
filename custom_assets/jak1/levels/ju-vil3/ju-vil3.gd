311faf97af71e23bf8f35ba10ba1e9a5c2da0f4e
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2024-06-23 23:15:12 -0700

    fix part despawn crash

--- custom_assets/jak1/levels/ju-vil3/ju-vil3.gd
+++ custom_assets/jak1/levels/ju-vil3/ju-vil3.gd
@@ -4,7 +4,7 @@
 ;; the actual file name still needs to be 8.3
 ("UPF.DGO"
  ("static-screen.o"
-  "training-part.o"
+  "village3-part.o"
   "villagep-obs.o"
   "warpgate-ag.go"
   "warp-gate-switch-ag.go"
